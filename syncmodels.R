396ce2d5ddb7c697dac8e1f448f65c38fc975d7f
lizzieinvancouver <wolkovich@biodiversity.ubc.ca>
2015-05-15 12:14:10 -0700

    Edits that did not make it in last commit

--- syncmodels.R
+++ syncmodels.R
@@ -3,8 +3,9 @@
 
 ## Trying to run STAN with synchrony data ##
 
-## Last updated on Tax Dax in the USA ##
+## Last updated 28 April 2015 ##
 
+rm(list=ls()) 
 options(stringsAsFactors = FALSE)
 
 setwd("~/Documents/git/projects/trophsynch/synchrony")
@@ -15,7 +16,29 @@ library(rstan)
 # get data
 # in the data file: spp1 = neg= negative species e.g. resource 
 raw <- read.csv("output/raw.csv", header=TRUE)
-rawlong <- read.csv("output/alldata_long.csv", header=TRUE)
+rawlong <- read.csv("input/indiv_sppdata2.csv", header=TRUE)
+rawlong$int_type[which(rawlong$int_type=="poll")] <- "pollination"
+rawlong.X <- NULL
+
+##
+# figure out how many unique species
+# and alter the data so each unique species shows up once
+# watch out: if you leave in int_type you still get duplicated species...
+# so it's not below until after duplicate species are removed!
+specieschar.wdups <- aggregate(rawlong["phenovalue"],
+    rawlong[c("studyid", "species", "spp")], FUN=length)
+specieschar <- aggregate(specieschar.wdups["phenovalue"],
+    specieschar.wdups[c("studyid", "species")], FUN=length)
+dupspp <- subset(specieschar, phenovalue>1)
+specieschar.wdups[which(specieschar.wdups$species %in% dupspp$species),]
+# delete duplicate species as spp1 (generally the shorter timeseries)
+rawlong.nodups <- rawlong[-(which(rawlong$species %in% dupspp$species &
+    rawlong$spp=="spp1")),]
+# and order it!
+rawlong.nodups <- rawlong.nodups[with(rawlong.nodups, order(species, year)),]
+specieschar.formodel <- aggregate(rawlong.nodups["phenovalue"],
+    rawlong.nodups[c("studyid", "species", "int_type", "spp")], FUN=length)
+
 
 ##
 ## lme4: fails to converge
@@ -23,46 +46,91 @@ rawlong <- read.csv("output/alldata_long.csv", header=TRUE)
 # modresource <- lmer(neg_phenovalue ~ year + (year|spp1), data=raw)
 
 ##
+# get estimate from no pooling (fixed effects) and complete pooling
+comppool <- lm(phenovalue~year, data=rawlong.nodups)
+comppool.wtype <- lm(phenovalue~year*spp-1, data=rawlong.nodups)
+nopool <- lm(phenovalue~year*species, data=rawlong.nodups)
+uniquespp <- unique(rawlong.nodups$species)
+lmfits <- rep(NA, length(uniquespp))
+for (eachsp in 1:length(uniquespp)){
+    lmhere <- lm(phenovalue~year, data=subset(rawlong.nodups, species==uniquespp[eachsp]))
+    lmfits[eachsp] <- coef(lmhere)[2]
+  }
+
+##
 ## try Stan #
 # 3 stages of cooking: prepared foods, recipes, make your own
 # Stan does not currently allow ragged arrays
 
+
 # prep the data to fit the model including:
 # aggregate to get species level characteristics
 # subset down to the phenovalues
-longpheno <- subset(rawlong, whatmeasured=="phenovalue")
-longpheno$yr1976 <- longpheno$year-1976
-specieschar <- aggregate(longpheno["measurement"],
-    longpheno[c("studyid", "value", "variable", "intid")], FUN=length)
-specieschar.nodup <- specieschar[!(duplicated(specieschar$value)==TRUE),] # there are some species with multiple relationships
+rawlong.nodups$yr1976 <- rawlong.nodups$year-1976
 
-N <- nrow(longpheno)
-y <- longpheno$measurement
-J <- nrow(specieschar.nodup)
-species <- as.numeric(as.factor(longpheno$value))
-year <- longpheno$yr1976
-type <- as.numeric(as.factor(specieschar.nodup$variable)) 
+N <- nrow(rawlong.nodups)
+y <- rawlong.nodups$phenovalue
+J <- nrow(specieschar.formodel)
+species <- as.numeric(as.factor(rawlong.nodups$species))
+year <- rawlong.nodups$yr1976
+type <- as.numeric(as.factor(specieschar.formodel$spp)) 
 
 # Run the model
-# source("source/stan.R") # fails for Lizzie: all scheduled cores encountered errors in user code
-fit1 <- stan("stan/synchrony1.stan", data=c("N","y","J","species","year","type"), iter=2000, chains=4)
+# source("source/stan.R")
+
+# Margaret Kosmala's model with no type
+fit.notype <- stan("stan/synchrony1_notype.stan", data=c("N","y","J","species","year"), iter=2000, chains=3)
+print(fit.notype)
+
+zz<-summary(fit.notype)
+intercepts.fitnotype.fromstan <- as.vector(zz[[1]][,1])[1:71]
+spptrends.fitnotype.fromstan <- as.vector(zz[[1]][,1])[72:142]
+
+# model with type (resource or consumer); the one and only original
+fit1 <- stan("stan/synchrony1.stan", data=c("N","y","J","species","year","type"), iter=1000, chains=4)
 
 library("shinyStan")
 launch_shinystan(fit1)
+
+# fit1 <- stan("stan/synchrony1_wpreds.stan", data=c("N","y","J","species","year","type"), iter=1000, chains=4)
+
 
 # Hack to look at the data
 qq<-summary(fit1)
 class(qq) # gives the overall summary and then each of the four chains (I think)
 qq[[1]][,1]
-spptrends.fromstan <- as.vector(qq[[1]][,1])[4:73]
-spptrends <- cbind(specieschar.nodup, spptrends.fromstan)
+spptrends.fromstan <- as.vector(qq[[1]][,1])[4:74]
+spptrends <- cbind(specieschar.formodel, spptrends.fromstan, lmfits)
 spptrends$doybydec <- spptrends$spptrends.fromstan*10
-  
-ggplot(data=spptrends, aes(x=variable, y=doybydec))+
-geom_path(aes(group=factor(intid)))+
-    #geom_point(aes(colour=factor(interaction), size=1))+ 
+spptrends$doybydec.lm <- spptrends$lmfits*10
+spptrends$doybydec.notype <- spptrends.fitnotype.fromstan*10
+
+ggplot(data=spptrends, aes(x=type, y=doybydec))+
+geom_path(aes(group=factor(studyid), colour=factor(int_type)))+
+    geom_point(aes(colour=factor(int_type), size=1))+ 
     theme_bw()+theme(panel.grid.major = element_blank(),
     axis.title.x =element_text(size=12), axis.text.x=element_text(size=12),
     axis.text.y=element_text(size=12), axis.title.y=element_text(size=12, angle=90))+
     ylab("Phenological shift (days/decade)")+xlab("Resource (1) or consumer (2)")+
     ggtitle("Slopes from Stan \n resources are on left, consumers on right (I think)")
+
+ggplot(data=spptrends, aes(x=type, y=doybydec.lm))+
+geom_path(aes(group=factor(studyid), colour=factor(int_type)))+
+    geom_point(aes(colour=factor(int_type), size=1))+ 
+    theme_bw()+theme(panel.grid.major = element_blank(),
+    axis.title.x =element_text(size=12), axis.text.x=element_text(size=12),
+    axis.text.y=element_text(size=12), axis.title.y=element_text(size=12, angle=90))+
+    ylab("Phenological shift (days/decade)")+xlab("Resource (1) or consumer (2)")+
+    ggtitle("Slopes from basic lm \n resources are on left, consumers on right (I think)")
+
+plot(doybydec~doybydec.lm, spptrends, ylab="Change in days per decade, from Stan",
+    xlab="Change in days per decade, simple lm", col="dodgerblue4")
+points(doybydec.notype~doybydec.lm, spptrends, pch=1, col="red")
+abline(0,1)
+abline((lm(doybydec~doybydec.lm, spptrends)), col="dodgerblue4")
+abline((lm(doybydec.notype~doybydec.lm, spptrends)), col="red")
+legend("topleft", legend=c("with type", "no type", "1:1"), pch=c(1,1,1),
+    col=c("dodgerblue4", "red", "black"), bty="n")
+
+
+write.csv(spptrends, "output/synchrony1spptrends.csv", row.names=FALSE)
