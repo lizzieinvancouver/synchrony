69beb20eca64af62204e514a2a27226dbdbd5724
Elizabeth Wolkovich <Lizzie@Elizabeths-MacBook-Pro.local>
2015-12-02 18:34:41 -0500

    Changed TOL codes a little for the few that did not match

--- input/phylogeny/hinchliff/phylogeny_scratch.R
+++ input/phylogeny/hinchliff/phylogeny_scratch.R
@@ -20,6 +20,12 @@ library(phytools)
 
 # hinchlifftree1 <- read.newick("input/phylogeny/hinchliff/fromdryad/draftversion3.tre") # too big!
 
+# Dan's approach to the big tree
+library(ade4)
+phtree <- scan("input/phylogeny/hinchliff/fromdryad/draftversion3.tre", what = "character")
+phtre <- newick2phylog(phtree) # Convert it ade4 phylog class
+spnames <- names(phtre$leaves) # Get species names
+
 # from supp, see section "Step 4: Prune the Taxonomy to Taxa Required by Phylogenetic Inputs."
 hinchlifftree <- read.newick("input/phylogeny/hinchliff/fromSupp/synth-v3-pruned-taxonomy.tre")
 
@@ -38,6 +44,20 @@ matchy <- getnamesnodup[match(tipnames, getnamesnodup$ott_idwott),]
 tree2<-tree
 tree2$tip.label<-as.vector(matchy$species)
 plot(tree2)
+
+## check for Beth
+
+for (i in seq_along(unique(mytips$ott_idwott))){
+    print(i)
+    print(grep(unique(mytips$ott_idwott)[i], phtree))
+}
+
+# not found (4 total) on first pass, now fixed
+grep("ott4311804", phtree)
+grep("ott5521218", phtree)
+grep("ott4921563", phtree)
+grep("ott4916717", phtree)
+
 
 ## Make a VCV
 library(geiger)
